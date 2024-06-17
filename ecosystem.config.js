32649926e8b99e6711d3b9b1caaa8e81b42423f0
duyvu871 <zoocp123@gmail.com>
2024-06-18 01:16:17 +0700

    update next-auth-url

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   apps : [{
-    script: 'set DEV_HOSTNAME=14.225.218.92&&set PORT=1124&&npx next start',
+    script: 'set DEV_HOSTNAME=14.225.218.92&&set PORT=1123&&npx next start',
     watch: '.',
    name: 'tin'
   }],
